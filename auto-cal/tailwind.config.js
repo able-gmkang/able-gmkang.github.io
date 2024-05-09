9d21e9fae7d5e95195bb5b9f73fd77fa72481254
Kang <gm.kang@ablelabsinc.com>
2024-05-09 14:22:11 +0900

    #kang update

---
+++ auto-cal/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#0099cc",
+        secondaryColor: "#0787b1",
+      },
+    },
+  },
+  plugins: [],
+};
